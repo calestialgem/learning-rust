332becd010b0ff2561d54470932b39937f8bdb22
Cem GEÇGEL <gecgelcem@outlook.com>
2022-01-31 18:17:20 +0300

    Aliases with use as

--- restaurant/src/lib.rs
+++ restaurant/src/lib.rs
@@ -7,10 +7,10 @@ mod front_of_house {
 }
 
 use self::front_of_house::hosting;
-use self::front_of_house::hosting::Visitors;
+use self::front_of_house::hosting::Visitors as Group;
 
 pub fn eat_at_restaurant() {
-    hosting::add_to_waitlist(Visitors(3));
-    hosting::add_to_waitlist(Visitors(2));
-    hosting::add_to_waitlist(Visitors(6));
+    hosting::add_to_waitlist(Group(3));
+    hosting::add_to_waitlist(Group(2));
+    hosting::add_to_waitlist(Group(6));
 }
