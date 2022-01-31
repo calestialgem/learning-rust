c13c436007b807abba246750cc368544c8b59fa8
Cem GEÇGEL <gecgelcem@outlook.com>
2022-01-31 07:20:55 +0300

    Arrays

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,10 +1,23 @@
 fn main() {
-    let tup = (500, 6.4, 1);
-    let together = ('C', 'E', 'M', 22, 9, 2000);
-    let (x, y, z) = tup;
-    println!("The value of y is: {}", y);
-    println!(
-        "Name: {}{}{} Birthday: {}.{}.{}",
-        together.0, together.1, together.2, together.3, together.4, together.5
-    );
+    let a = [1, 2, 3, 4, 5];
+    let months = [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December",
+    ];
+    let a: [i32; 5] = [1, 2, 3, 4, 5];
+    println!("Array: [{}, {}, {}, {}, {}]", a[0], a[1], a[2], a[3], a[4]);
+    let first = a[0];
+    let second = a[1];
+    let a = [3; 5];
+    println!("Array: [{}, {}, {}, {}, {}]", a[0], a[1], a[2], a[3], a[4]);
 }
