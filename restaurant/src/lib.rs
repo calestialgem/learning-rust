2f398e6e9df106d8d168bc7d1b6770f8c86459ff
Cem GEÇGEL <gecgelcem@outlook.com>
2022-01-31 18:13:40 +0300

    Struct with use

--- restaurant/src/lib.rs
+++ restaurant/src/lib.rs
@@ -1,13 +1,16 @@
 mod front_of_house {
     pub mod hosting {
-        pub fn add_to_waitlist() {}
+        pub struct Visitors(pub u32);
+
+        pub fn add_to_waitlist(group: Visitors) {}
     }
 }
 
 use self::front_of_house::hosting;
+use self::front_of_house::hosting::Visitors;
 
 pub fn eat_at_restaurant() {
-    hosting::add_to_waitlist();
-    hosting::add_to_waitlist();
-    hosting::add_to_waitlist();
+    hosting::add_to_waitlist(Visitors(3));
+    hosting::add_to_waitlist(Visitors(2));
+    hosting::add_to_waitlist(Visitors(6));
 }
