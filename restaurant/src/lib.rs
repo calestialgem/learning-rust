d199c15d388aa2debc18b8e9767582d4642b6a5a
Cem GEÇGEL <gecgelcem@outlook.com>
2022-01-31 18:25:01 +0300

    Import Everything with use *

--- restaurant/src/lib.rs
+++ restaurant/src/lib.rs
@@ -2,14 +2,14 @@ mod front_of_house {
     pub mod hosting {
         pub struct Visitors(pub u32);
 
-        pub fn add_to_waitlist(group: crate::Group) {}
+        pub fn add_to_waitlist(group: Visitors) {}
     }
 }
 
-use self::front_of_house::hosting::{self, Visitors as Group};
+use self::front_of_house::hosting::{self, *};
 
 pub fn eat_at_restaurant() {
-    hosting::add_to_waitlist(Group(3));
-    hosting::add_to_waitlist(Group(2));
-    hosting::add_to_waitlist(Group(6));
+    hosting::add_to_waitlist(Visitors(3));
+    hosting::add_to_waitlist(Visitors(2));
+    hosting::add_to_waitlist(Visitors(6));
 }
