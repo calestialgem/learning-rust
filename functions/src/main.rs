6e1a69f5c5c90a1d85666c3e1d22914157aca1dd
Cem GEÇGEL <gecgelcem@outlook.com>
2022-01-31 07:38:19 +0300

    Comments

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -1,15 +1,20 @@
 fn main() {
+    // y binds to the result of the block expression
     let y = {
         let x = five();
+        // there is not a semicolon here
+        // this is the returned expression
         plus_one(x)
     };
     println!("The value of y is: {}", y);
 }
 
+// the return type is defined after arrow
 fn five() -> i32 {
     5
 }
 
 fn plus_one(x: i32) -> i32 {
+    // the return expression can be given at the end
     x + 1
 }
