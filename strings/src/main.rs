dc4f268fbd79c334443b19ccfb28f7013e0f2f84
Cem GEÇGEL <gecgelcem@outlook.com>
2022-01-31 22:11:03 +0300

    String Concatination

--- strings/src/main.rs
+++ strings/src/main.rs
@@ -1,6 +1,10 @@
 fn main() {
     let data = "initial contents";
-    let s = data.to_string();
-    let s = "initial contents".to_string();
-    let hello = String::from("Merhaba!");
+    let mut s = data.to_string();
+    s.push_str(" some other contents ile this character: ");
+    s.push('l');
+
+    let s1 = String::from("Hello, ");
+    let s2 = String::from(" world!");
+    println!("{}", s1 + &s2);
 }
