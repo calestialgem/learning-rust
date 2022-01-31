6e28e9347ff3bfc67eac26273c4f5716a8884718
Cem GEÇGEL <gecgelcem@outlook.com>
2022-01-31 23:15:09 +0300

    Indexing Strings

--- strings/src/main.rs
+++ strings/src/main.rs
@@ -1,20 +1,5 @@
 fn main() {
-    let first_name = String::from("Cem");
-    let second_name = String::from("Geçgel");
-    let gender = String::from("Male");
-    let nationality = String::from("Turkish");
-    let birth_day = 22;
-    let birth_month = String::from("September");
-    let birth_year = 2000;
-    let identity = format!(
-        "{} {}\n [*] Birth: {} {} {}\n [*] Gender: {}\n [*] Nationality: {}",
-        first_name,
-        second_name.to_uppercase(),
-        birth_day,
-        birth_month,
-        birth_year,
-        gender,
-        nationality
-    );
-    println!("{}", identity);
+    let name = String::from("Cem Geçgel");
+    println!("{}", &name[6..8]);
+    println!("{}", &name[6..7]);
 }
