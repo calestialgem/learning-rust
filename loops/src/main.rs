953ef2807d75061cb4839b53a6d4bb80a4ddd56f
Cem GEÇGEL <gecgelcem@outlook.com>
2022-01-31 08:26:19 +0300

    For with Ranges

--- loops/src/main.rs
+++ loops/src/main.rs
@@ -1,12 +1,6 @@
 fn main() {
-    let a = [3, -1, 55, 17, -33];
-    let result = {
-        let mut accumulator = 0;
-        for value in a {
-            accumulator += value;
-        }
-        accumulator
-    };
-
-    println!("The result is: {}", result);
+    for count in (1..4).rev() {
+        println!("{}!", count);
+    }
+    println!("LIFTOFF!!!");
 }
