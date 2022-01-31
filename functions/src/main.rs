3d7a72db943133a6e6d0afc338e6efd92ab853b3
Cem GEÇGEL <gecgelcem@outlook.com>
2022-01-31 07:34:58 +0300

    Returning From Functions

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -1,7 +1,15 @@
 fn main() {
     let y = {
-        let x = 3;
-        x + 1
+        let x = five();
+        plus_one(x)
     };
     println!("The value of y is: {}", y);
+}
+
+fn five() -> i32 {
+    5
+}
+
+fn plus_one(x: i32) -> i32 {
+    x + 1
 }
