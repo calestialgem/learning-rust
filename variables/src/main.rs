81ee63e0e8ad30b6b4afadb310cb825e4e23af0e
Cem GEÇGEL <gecgelcem@outlook.com>
2022-01-31 07:02:39 +0300

    Numeric Operations

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,5 +1,8 @@
 fn main() {
-    let spaces = "   ";
-    let spaces = spaces.len();
-    println!("The amount of spaces: {}", spaces);
+    let sum = 5 + 10;
+    let difference = 95.5 - 4.3;
+    let product = 4 * 30;
+    let quotient = 56.7 / 32.2;
+    let floored = 2 / 3;
+    let remainder = 43 % 5;
 }
