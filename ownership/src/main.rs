bcb93cfe65804ada9baecb9873451f10a360cf10
Cem GEÇGEL <gecgelcem@outlook.com>
2022-01-31 09:11:55 +0300

    Tuples for Returning the Result and the Ownership

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,17 +1,10 @@
 fn main() {
-    let s1 = gives_ownership();
-    let s2 = String::from("hello");
-    let s3 = takes_and_gives_back(s2);
-    // s2 is moved out from, it is invalid
-} // s3, then s2 are dropped
+    let s1 = String::from("hello");
+    let (len, s2) = calculate_length(s1);
+    println!("The length of '{}' is {}.", s2, len);
+}
 
-fn gives_ownership() -> String {
-    let some_string = String::from("yours");
-    some_string
-    // the string is moved out to the calling function
-} // it is not dropped here as it is invalid
-
-fn takes_and_gives_back(a_string: String) -> String {
-    a_string
-    // the string is moved out to the calling function
-} // it is not dropped here as it is invalid
+// Tuples can be used for returning both the result and the ownership
+fn calculate_length(s: String) -> (usize, String) {
+    (s.len(), s)
+}
