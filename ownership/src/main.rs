4b59d10b5d225365c9ccd2eaad2ec8f25ef90320
Cem GEÇGEL <gecgelcem@outlook.com>
2022-01-31 09:54:46 +0300

    General Slices

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,15 +1,5 @@
 fn main() {
-    let s = String::from("hello world");
-    let word = first_word(&s);
-    println!("First word in '{}' is '{}'.", s, word);
-}
-
-fn first_word(s: &str) -> &str {
-    let bytes = s.as_bytes();
-    for (i, &item) in bytes.iter().enumerate() {
-        if item == b' ' {
-            return &s[..i];
-        }
-    }
-    &s[..]
+    let a = [1, 2, 3, 4, 5];
+    let slice = &a[1..3];
+    assert_eq!(slice, &[2, 3]);
 }
