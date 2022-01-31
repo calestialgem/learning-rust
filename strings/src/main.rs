14a51357416d2b99bb311a8c01aff66d46ab4768
Cem GEÇGEL <gecgelcem@outlook.com>
2022-01-31 22:11:18 +0300

    String Formatting

--- strings/src/main.rs
+++ strings/src/main.rs
@@ -1,10 +1,20 @@
 fn main() {
-    let data = "initial contents";
-    let mut s = data.to_string();
-    s.push_str(" some other contents ile this character: ");
-    s.push('l');
-
-    let s1 = String::from("Hello, ");
-    let s2 = String::from(" world!");
-    println!("{}", s1 + &s2);
+    let first_name = String::from("Cem");
+    let second_name = String::from("Geçgel");
+    let gender = String::from("Male");
+    let nationality = String::from("Turkish");
+    let birth_day = 22;
+    let birth_month = String::from("September");
+    let birth_year = 2000;
+    let identity = format!(
+        "{} {}\n [*] Birth: {} {} {}\n [*] Gender: {}\n [*] Nationality: {}",
+        first_name,
+        second_name.to_uppercase(),
+        birth_day,
+        birth_month,
+        birth_year,
+        gender,
+        nationality
+    );
+    println!("{}", identity);
 }
