8d6a897c6b9f3ad7b10eeb1eba9259d30a958165
Cem GEÇGEL <gecgelcem@outlook.com>
2022-01-31 07:13:56 +0300

    Tuples

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,5 +1,10 @@
 fn main() {
-    let c = 'z';
-    let z = 'Z';
-    let heart_eyed_cat = '😻';
+    let tup = (500, 6.4, 1);
+    let together = ('C', 'E', 'M', 22, 9, 2000);
+    let (x, y, z) = tup;
+    println!("The value of y is: {}", y);
+    println!(
+        "Name: {}{}{} Birthday: {}.{}.{}",
+        together.0, together.1, together.2, together.3, together.4, together.5
+    );
 }
