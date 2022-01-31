cdb764545ab233f2d9fea8e13068cb152fb53fd6
Cem GEÇGEL <gecgelcem@outlook.com>
2022-01-31 09:03:12 +0300

    Function Argument Ownership

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,5 +1,16 @@
 fn main() {
-    let s1 = String::from("hello");
-    let s2 = s1.clone();
-    println!("s1: {}\ns2: {}", s1, s2);
+    let s = String::from("hello");
+    takes_ownership(s);
+    // the string is invalid: it is moved from
+    let x = 5;
+    makes_copy(x);
+    // the number is still valid
+} // the number, then the string, are out of scope, but nothing is dropped
+
+fn takes_ownership(some_string: String) {
+    println!("{}", some_string);
+} // the string is dropped here
+
+fn makes_copy(some_integer: i32) {
+    println!("{}", some_integer);
 }
