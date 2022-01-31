31dcf6bedb61b4d3c067b63084ae2bc988eb0829
Cem GEÇGEL <gecgelcem@outlook.com>
2022-01-31 08:22:49 +0300

    Looping Over Arrays

--- loops/src/main.rs
+++ loops/src/main.rs
@@ -1,10 +1,11 @@
 fn main() {
+    let a = [3, -1, 55, 17, -33];
     let result = {
         let mut counter = 0;
         let mut accumulator = 0;
-        while counter != 10 {
+        while counter < 5 {
+            accumulator += a[counter];
             counter += 1;
-            accumulator += counter;
         }
         accumulator
     };
