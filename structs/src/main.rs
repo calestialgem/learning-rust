8f782abe49a2e5fdbf4435bda58cfbc1f7fb1d51
Cem GEÇGEL <gecgelcem@outlook.com>
2022-01-31 10:05:00 +0300

    Build Function

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -5,13 +5,20 @@ struct User {
     sign_in_count: u64,
 }
 
-fn main() {
-    let mut user1 = User {
-        email: String::from("someone@example.com"),
-        username: String::from("someusername123"),
+fn build_user(email: String, username: String) -> User {
+    User {
+        email: email,
+        username: username,
         active: true,
         sign_in_count: 1,
-    };
+    }
+}
+
+fn main() {
+    let mut user1 = build_user(
+        String::from("someone@example.com"),
+        String::from("someusername123"),
+    );
     user1.email = String::from("anotheremail@example.com");
     println!(
         "User: {} <{}> ({}) {}",
