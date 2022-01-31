e731567224eed2ec19c9f1c58c2ccfded86062e2
Cem GEÇGEL <gecgelcem@outlook.com>
2022-01-31 08:09:51 +0300

    else-if

--- branches/src/main.rs
+++ branches/src/main.rs
@@ -1,9 +1,11 @@
 fn main() {
-    let number = 3;
+    let number = 5;
     println!(
         "if-else expression: {}",
         if number < 5 {
             "conditions was true"
+        } else if number == 5 {
+            "!!THE THIRD OPTION!!"
         } else {
             "conditions was false"
         }
