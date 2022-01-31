9969b349ca47b001d0cb015e091914389509eb7c
Cem GEÇGEL <gecgelcem@outlook.com>
2022-01-31 10:16:44 +0300

    Tuple Structs

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -1,39 +1,10 @@
-struct User {
-    active: bool,
-    username: String,
-    email: String,
-    sign_in_count: u64,
-}
-
-fn build_user(email: String, username: String) -> User {
-    User {
-        email,    // field init shorthand
-        username, // field init shorthand
-        active: true,
-        sign_in_count: 1,
-    }
-}
+struct Vector(f32, f32);
 
 fn main() {
-    let user1 = build_user(
-        String::from("someone@example.com"),
-        String::from("someusername123"),
-    );
-    let user2 = User {
-        active: user1.active,
-        username: user1.username,
-        email: String::from("another@example.com"),
-        sign_in_count: user1.sign_in_count,
-    };
-    let user3 = User {
-        email: String::from("anotheremail@example.com"),
-        ..user2 // struct update syntax
-    };
+    let velocity = Vector(0.0, 0.0);
+    let acceleration = Vector(0.0, -9.81);
     println!(
-        "User: {} <{}> ({}) {}",
-        user3.username,
-        user3.email,
-        user3.sign_in_count,
-        if user3.active { "Online" } else { "Offline" }
+        "The vertical velocity would be {} m/s after one 1 second at {} m down from the initial position.",
+        velocity.1 + acceleration.1, -acceleration.1/2.0
     );
 }
