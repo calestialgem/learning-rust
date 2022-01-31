52aa58f1438636596f16bc8af514f19e36700947
Cem GEÇGEL <gecgelcem@outlook.com>
2022-01-31 07:30:50 +0300

    Scope Block Expressions

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -1,7 +1,7 @@
 fn main() {
-    another_function(5, 'h');
-}
-
-fn another_function(value: i32, unit_label: char) {
-    println!("The measurement is: {}{}", value, unit_label);
+    let y = {
+        let x = 3;
+        x + 1
+    };
+    println!("The value of y is: {}", y);
 }
