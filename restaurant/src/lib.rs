21e062842da64176370d872568656e938213a25e
Cem GEÇGEL <gecgelcem@outlook.com>
2022-01-31 18:19:57 +0300

    Re-exporting with pub use

--- restaurant/src/lib.rs
+++ restaurant/src/lib.rs
@@ -2,12 +2,12 @@ mod front_of_house {
     pub mod hosting {
         pub struct Visitors(pub u32);
 
-        pub fn add_to_waitlist(group: Visitors) {}
+        pub fn add_to_waitlist(group: crate::Group) {}
     }
 }
 
 use self::front_of_house::hosting;
-use self::front_of_house::hosting::Visitors as Group;
+pub use self::front_of_house::hosting::Visitors as Group;
 
 pub fn eat_at_restaurant() {
     hosting::add_to_waitlist(Group(3));
