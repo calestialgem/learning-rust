31a4811a10eb7a6d4c46b25875237886484a4f58
Cem GEÇGEL <gecgelcem@outlook.com>
2022-01-31 08:23:40 +0300

    For Loop

--- loops/src/main.rs
+++ loops/src/main.rs
@@ -1,11 +1,9 @@
 fn main() {
     let a = [3, -1, 55, 17, -33];
     let result = {
-        let mut counter = 0;
         let mut accumulator = 0;
-        while counter < 5 {
-            accumulator += a[counter];
-            counter += 1;
+        for value in a {
+            accumulator += value;
         }
         accumulator
     };
