4f0cb75919740b4c65c5d89bf08907e790c6c34b
Cem GEÇGEL <gecgelcem@outlook.com>
2022-01-31 10:20:26 +0300

    Unit-Like Structs

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -1,10 +1,6 @@
-struct Vector(f32, f32);
-
 fn main() {
-    let velocity = Vector(0.0, 0.0);
-    let acceleration = Vector(0.0, -9.81);
-    println!(
-        "The vertical velocity would be {} m/s after one 1 second at {} m down from the initial position.",
-        velocity.1 + acceleration.1, -acceleration.1/2.0
-    );
+    struct AlwaysEqual; // unit-like struct
+    struct AlwaysNotEqual(); // which is a special tuple struct
+    let subject = AlwaysEqual;
+    let subject = AlwaysNotEqual();
 }
