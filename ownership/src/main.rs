fecfa4f0570d80dac0a82eb7fe2ee15e20460593
Cem GEÇGEL <gecgelcem@outlook.com>
2022-01-31 09:15:05 +0300

    Barrowing with References

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,10 +1,10 @@
 fn main() {
     let s1 = String::from("hello");
-    let (len, s2) = calculate_length(s1);
-    println!("The length of '{}' is {}.", s2, len);
+    let len = calculate_length(&s1);
+    println!("The length of '{}' is {}.", s1, len);
 }
 
-// Tuples can be used for returning both the result and the ownership
-fn calculate_length(s: String) -> (usize, String) {
-    (s.len(), s)
+// The references can barrow ownership.
+fn calculate_length(s: &String) -> usize {
+    s.len()
 }
