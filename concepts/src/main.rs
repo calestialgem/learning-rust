7253cb1d928e0ea029adf86823e07c5bbcb7c53d
Cem GEÇGEL <gecgelcem@outlook.com>
2022-02-01 23:45:40 +0300

    Generic with Traits

--- concepts/src/main.rs
+++ concepts/src/main.rs
@@ -12,7 +12,7 @@ fn main() {
     println!("The largest number is {}", result);
 }
 
-fn largest<T>(list: &[T]) -> T {
+fn largest<T: PartialOrd + Copy>(list: &[T]) -> T {
     let mut largest = list[0];
     for &item in list {
         if item > largest {
