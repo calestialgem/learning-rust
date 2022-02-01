e19b9d6176e951c3ff0e37507afe1fedbd35e55b
Cem GEÇGEL <gecgelcem@outlook.com>
2022-02-01 23:28:12 +0300

    Generic Method Does Not Work

--- concepts/src/main.rs
+++ concepts/src/main.rs
@@ -1,28 +1,18 @@
 fn main() {
     let number_list = vec![34, 50, 25, 100, 65];
-    let result = largest_i32(&number_list);
+    let result = largest(&number_list);
     println!("The largest number is {}", result);
 
     let number_list = vec![102, 34, 6000, 89, 54, 2, 43, 8];
-    let result = largest_i32(&number_list);
+    let result = largest(&number_list);
     println!("The largest number is {}", result);
 
     let number_list = vec![-0.32, 66.66, 3.1419, 2.7, -7580.0];
-    let result = largest_f32(&number_list);
+    let result = largest(&number_list);
     println!("The largest number is {}", result);
 }
 
-fn largest_i32(list: &[i32]) -> i32 {
-    let mut largest = list[0];
-    for &item in list {
-        if item > largest {
-            largest = item;
-        }
-    }
-    largest
-}
-
-fn largest_f32(list: &[f32]) -> f32 {
+fn largest<T>(list: &[T]) -> T {
     let mut largest = list[0];
     for &item in list {
         if item > largest {
