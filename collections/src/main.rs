1af854dd3668520e1dc3e2f10ee010ed5522776b
Cem GEÇGEL <gecgelcem@outlook.com>
2022-02-01 17:35:23 +0300

    Updating a Hash Map

--- collections/src/main.rs
+++ collections/src/main.rs
@@ -1,35 +1,70 @@
-#[derive(Debug)]
-enum DaysOfWeek {
-    Monday,
-    Tuesday,
-    Wednesdey,
-    Thursday,
-    Friday,
-    Saturday,
-    Sunday,
-}
+use std::collections::HashMap;
 
 fn main() {
-    use std::collections::HashMap;
-    use DaysOfWeek::*;
-    let mut work_time = HashMap::new();
-    work_time.insert(
-        String::from("Full-Time"),
-        vec![Monday, Tuesday, Thursday, Friday],
+    let mut weekly = HashMap::new();
+    weekly.insert("Monday", 12);
+    weekly.insert("Tuesday", 12);
+    weekly.insert("Thursday", 12);
+    weekly.insert("Friday", 12);
+    weekly.insert("Sunday", 6);
+    print_all(&weekly);
+    print("Monday", &weekly);
+    overwriting_a_value(&mut weekly);
+    only_inserting_a_value_if_the_key_has_no_value(&mut weekly);
+    updating_a_value_based_on_the_old_value(&mut weekly);
+}
+
+fn print(day: &str, weekly: &HashMap<&str, i32>) {
+    println!(
+        "On {}s, I work {} hours.",
+        day,
+        weekly
+            .get(day)
+            .expect("Could not find information on the given day!")
     );
-    work_time.insert(String::from("Half-Time"), vec![Saturday]);
-    work_time.insert(String::from("Never"), vec![Wednesdey, Sunday]);
-    let free_days = work_time
-        .get(&String::from("Never"))
-        .expect("Working time 'never', is not provided.");
-    for d in free_days {
-        print!("{:?} ", d);
+}
+
+fn print_all(weekly: &HashMap<&str, i32>) {
+    println!("Weekly working time: ");
+    for (d, h) in weekly {
+        println!("[*] {}: {}", d, h);
     }
-    println!("are free for me!");
-    println!("Working full-time in: ");
-    if let Some(days) = work_time.get(&String::from("Full-Time")) {
-        for d in days {
-            println!(" [*] {:?}", d);
-        }
+}
+
+fn overwriting_a_value(weekly: &mut HashMap<&str, i32>) {
+    weekly.insert("Monday", 17);
+    print("Monday", &weekly);
+}
+
+fn only_inserting_a_value_if_the_key_has_no_value(weekly: &mut HashMap<&str, i32>) {
+    for d in [
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+        "Sunday",
+    ] {
+        weekly.entry(d).or_insert(0);
     }
+    print_all(weekly);
+}
+
+fn updating_a_value_based_on_the_old_value(weekly: &mut HashMap<&str, i32>) {
+    let extra = {
+        let mut extra = HashMap::new();
+        extra.insert("Monday", 5);
+        extra.insert("Tuesday", 5);
+        extra.insert("Wednesday", 12);
+        extra.insert("Thursday", 5);
+        extra.insert("Friday", 5);
+        extra.insert("Saturday", 12);
+        extra
+    };
+    for (day, hours) in extra {
+        let weekly_day = weekly.entry(day).or_insert(0);
+        *weekly_day += hours;
+    }
+    print_all(weekly);
 }
