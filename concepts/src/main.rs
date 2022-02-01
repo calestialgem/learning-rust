35426e71b4699b69c0af3ec68b8b0cf5ecb1b7f0
Cem GEÇGEL <gecgelcem@outlook.com>
2022-02-01 23:55:11 +0300

    Initial Code

--- concepts/src/main.rs
+++ concepts/src/main.rs
@@ -1,23 +1,9 @@
-fn main() {
-    let number_list = vec![34, 50, 25, 100, 65];
-    let result = largest(&number_list);
-    println!("The largest number is {}", result);
+fn main() {}
 
-    let number_list = vec![102, 34, 6000, 89, 54, 2, 43, 8];
-    let result = largest(&number_list);
-    println!("The largest number is {}", result);
-
-    let number_list = vec![-0.32, 66.66, 3.1419, 2.7, -7580.0];
-    let result = largest(&number_list);
-    println!("The largest number is {}", result);
-}
-
-fn largest<T: PartialOrd + Copy>(list: &[T]) -> T {
-    let mut largest = list[0];
-    for &item in list {
-        if item > largest {
-            largest = item;
-        }
+fn longest(x: &str, y: &str) -> &str {
+    if x.len() > y.len() {
+        x
+    } else {
+        y
     }
-    largest
 }
