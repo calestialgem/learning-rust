ab4e34f1b138eeb15334e203d9b80b0670fa8d83
Cem GEÇGEL <gecgelcem@outlook.com>
2022-02-02 14:05:36 +0300

    My Own Tests

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -24,3 +24,27 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     println!("With text:\n{}", contents);
     Ok(())
 }
+
+#[cfg(test)]
+mod test_config {
+    use super::*;
+
+    #[test]
+    fn three_arguments() {
+        let args = [
+            String::from("running from test"),
+            String::from("Hello"),
+            String::from("World"),
+        ];
+        let config = Config::new(&args).expect("Could not create config?");
+        assert_eq!(args[1], config.query);
+        assert_eq!(args[2], config.filename);
+    }
+
+    #[test]
+    #[should_panic(expected = "not enough arguments")]
+    fn less_than_three_arguments() {
+        let args = [String::from("Hello")];
+        Config::new(&args).unwrap();
+    }
+}
