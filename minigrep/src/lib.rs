b75ad0809be72933efd025668460c9ae9d74ec1b
Cem GEÇGEL <gecgelcem@outlook.com>
2022-02-02 15:34:26 +0300

    Implemented Case Insensetive Search

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -38,9 +38,10 @@ pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
 }
 
 pub fn search_case_insensetive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let query = query.to_lowercase();
     let mut result = Vec::new();
     for line in contents.lines() {
-        if line.contains(query) {
+        if line.to_lowercase().contains(&query) {
             result.push(line);
         }
     }
