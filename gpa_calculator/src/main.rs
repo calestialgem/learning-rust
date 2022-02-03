2a392b091ff83f8c6cde86ef578bb2d8ec67650b
Cem GEÇGEL <gecgelcem@outlook.com>
2022-02-03 21:54:04 +0300

    Crated Grades

--- gpa_calculator/src/main.rs
+++ gpa_calculator/src/main.rs
@@ -1,3 +1,29 @@
+enum Grade {
+    Aa,
+    Ba,
+    Bb,
+    Cb,
+    Cc,
+    Dc,
+    Dd,
+    F,
+}
+
+impl Grade {
+    fn points(&self) -> u32 {
+        match *self {
+            Grade::Aa => 40,
+            Grade::Ba => 35,
+            Grade::Bb => 30,
+            Grade::Cb => 25,
+            Grade::Cc => 20,
+            Grade::Dc => 15,
+            Grade::Dd => 10,
+            Grade::F => 0,
+        }
+    }
+}
+
 fn main() {
     println!("Hello, world!");
 }
