97d1bec638bcce81dc57fee7c616155abd0d2ceb
Cem GEÇGEL <gecgelcem@outlook.com>
2022-02-03 23:19:46 +0300

    Used Fixed Point Calculations

--- gpa_calculator/src/main.rs
+++ gpa_calculator/src/main.rs
@@ -4,30 +4,56 @@ struct Points {
 }
 
 impl Points {
-    const scale: u32 = 1000;
-    const half: u32 = Self::scale / 2;
+    const SCALE: u32 = 1000;
+    const HALF: u32 = Self::SCALE / 2;
+    const ZERO: Points = Points::new(0, 0);
 
-    fn new(whole: u32, fraction: u32) -> Self {
+    const fn new(whole: u32, fraction: u32) -> Self {
         Self {
-            whole: whole + fraction / Self::scale,
-            fraction: fraction % Self::scale,
+            whole: whole + fraction / Self::SCALE,
+            fraction: fraction % Self::SCALE,
         }
     }
 
     fn as_fraction(&self) -> u32 {
-        self.whole * Self::scale + self.fraction
+        self.whole * Self::SCALE + self.fraction
     }
 
     fn add(&self, other: &Self) -> Self {
         Self::new(self.whole + other.whole, self.fraction + other.fraction)
     }
 
-    fn mul(&self, other: &Self) -> Self {
-        Self::new(0, self.as_fraction() * other.as_fraction() / Self::scale)
+    fn mul(&self, scalar: u32) -> Self {
+        Self::new(0, self.as_fraction() * scalar)
     }
 
-    fn div(&self, other: u32) -> Self {
-        Self::new(0, self.as_fraction() / other)
+    fn div(&self, scalar: u32) -> Self {
+        Self::new(0, self.as_fraction() / scalar)
+    }
+
+    fn round(&self) -> Self {
+        let round_up = self.fraction % 10 >= 5;
+        let tens = self.fraction / 10 + round_up as u32;
+        Self::new(self.whole, tens * 10)
+    }
+
+    fn print(&self) {
+        if self.fraction == 0 {
+            print!("{}", self.whole);
+        } else {
+            let mut scale = Self::SCALE;
+            loop {
+                scale /= 10;
+                if scale < 10 {
+                    print!("{}.{}", self.whole, self.fraction);
+                    break;
+                }
+                if self.fraction % scale == 0 {
+                    print!("{}.{}", self.whole, self.fraction / scale);
+                    break;
+                }
+            }
+        }
     }
 }
 
@@ -43,30 +69,30 @@ enum Grade {
 }
 
 impl Grade {
-    fn points(&self) -> u32 {
+    fn points(&self) -> Points {
         match *self {
-            Grade::Aa => 4000,
-            Grade::Ba => 3500,
-            Grade::Bb => 3000,
-            Grade::Cb => 2500,
-            Grade::Cc => 2000,
-            Grade::Dc => 1500,
-            Grade::Dd => 1000,
-            Grade::F => 0,
+            Grade::Aa => Points::new(4, 0),
+            Grade::Ba => Points::new(3, Points::HALF),
+            Grade::Bb => Points::new(3, 0),
+            Grade::Cb => Points::new(2, Points::HALF),
+            Grade::Cc => Points::new(2, 0),
+            Grade::Dc => Points::new(1, Points::HALF),
+            Grade::Dd => Points::new(1, 0),
+            Grade::F => Points::ZERO,
         }
     }
 }
 
 struct Course {
     credits: u32,
-    points: u32,
+    points: Points,
 }
 
 impl Course {
     fn new(grade: Grade, credits: u32) -> Self {
         Course {
             credits,
-            points: grade.points() * credits,
+            points: grade.points().mul(credits),
         }
     }
 }
@@ -74,60 +100,64 @@ impl Course {
 struct Semester {
     name: String,
     credits: u32,
-    points: u32,
-    average: u32,
+    points: Points,
+    average: Points,
 }
 
 impl Semester {
     fn new(name: String, courses: &[Course]) -> Self {
         let mut credits = 0;
-        let mut points = 0;
+        let mut points = Points::ZERO;
         for course in courses {
             credits += course.credits;
-            points += course.points;
+            points = points.add(&course.points);
         }
+        let average = points.div(credits);
         Semester {
             name,
             credits,
             points,
-            average: points / credits,
+            average,
         }
     }
 
     fn print(&self) {
-        println!(
-            "[{}] Credits: {}, Points: {}.{}, SPA: {}",
-            self.name, self.credits, self.points, self.average
-        );
+        print!("[{}] Credits: {}, Points: ", self.name, self.credits);
+        self.points.round().print();
+        print!(", SPA: ");
+        self.average.round().print();
+        println!();
     }
 }
 
 struct Overall {
     credits: u32,
-    points: u32,
-    average: u32,
+    points: Points,
+    average: Points,
 }
 
 impl Overall {
     fn new(semesters: &[Semester]) -> Self {
         let mut credits = 0;
-        let mut points = 0;
+        let mut points = Points::ZERO;
         for semester in semesters {
             credits += semester.credits;
-            points += semester.points;
+            points = points.add(&semester.points);
         }
+        let average = points.div(credits);
         Overall {
             credits,
             points,
-            average: points / credits,
+            average,
         }
     }
 
     fn print(&self) {
-        println!(
-            "[Overall] Credits: {}, Points: {}, GPA: {}",
-            self.credits, self.points, self.average
-        );
+        print!("[Overall] Credits: {}, Points: ", self.credits);
+        self.points.round().print();
+        print!(", SPA: ");
+        self.average.round().print();
+        println!();
     }
 }
 
