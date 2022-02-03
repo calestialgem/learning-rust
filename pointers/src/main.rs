814de2fbdf08d825e4ddf87c2a16323f8bfe8a38
Cem GEÇGEL <gecgelcem@outlook.com>
2022-02-03 17:58:10 +0300

    Using Deref

--- pointers/src/main.rs
+++ pointers/src/main.rs
@@ -1,5 +1,28 @@
+struct MyBox<T>(T);
+
+impl<T> MyBox<T> {
+    fn new(x: T) -> MyBox<T> {
+        MyBox(x)
+    }
+}
+
+use std::ops::Deref;
+
+impl<T> Deref for MyBox<T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
 fn main() {
     let x = 534_500_000;
-    let y = Box::new(x);
-    println!("Value 0x{:08x} in Address {:016p}", *y, y);
+    let y = MyBox::new(x);
+    println!(
+        "Value 0x{:08x} in Address {:016p} or with Deref {:016p}",
+        *y, // short of *(y.deref())
+        &y, // short of y.deref()
+        y.deref()
+    );
 }
