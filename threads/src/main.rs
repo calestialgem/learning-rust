9ab858e92303be9a7bf557189ce5ae8a870fb517
Cem GEÇGEL <gecgelcem@outlook.com>
2022-02-04 10:12:17 +0300

    Mutex

--- threads/src/main.rs
+++ threads/src/main.rs
@@ -1,40 +1,13 @@
-use std::sync::mpsc;
-use std::thread;
-use std::time::Duration;
+use std::sync::Mutex;
 
 fn main() {
-    let (tx1, rx) = mpsc::channel(); // multiple producer, single consumer
-    let tx2 = tx1.clone(); // second producer
-                           // first thread
-    thread::spawn(move || {
-        let vals = vec![
-            String::from("hi"),
-            String::from("from"),
-            String::from("the"),
-            String::from("thread"),
-        ];
-        for val in vals {
-            tx1.send(val).unwrap();
-            thread::sleep(Duration::from_secs(1));
-        }
-    });
-    // second thread
-    thread::spawn(move || {
-        let vals = vec![
-            String::from("more"),
-            String::from("messages"),
-            String::from("for"),
-            String::from("you"),
-        ];
-        for val in vals {
-            tx2.send(val).unwrap();
-            thread::sleep(Duration::from_secs(1));
-        }
-    });
-
-    // iteration ends when the channel is closed
-    // until then it waits for any data
-    for received in rx {
-        println!("Got: {}", received);
+    let m = Mutex::new(5);
+    {
+        // returns a LockResult of MutexGuard
+        // this smart pointer returns the lock when dropped
+        let mut num = m.lock().unwrap();
+        *num = 6;
     }
+
+    println!("m = {:?}", m);
 }
