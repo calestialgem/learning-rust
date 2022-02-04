c3e4ca223b560943951b9268f2056ed7359ece85
Cem GEÇGEL <gecgelcem@outlook.com>
2022-02-04 23:36:12 +0300

    Started Handling Connection Requests

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -1,11 +1,16 @@
-use std::net::TcpListener;
+use std::io::prelude::*;
+use std::net::{TcpListener, TcpStream};
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
-
-    for stream in listener.incoming() {
-        let stream = stream.unwrap();
-
-        println!("Connection established");
+    for req in listener.incoming() {
+        let req = req.unwrap();
+        con_req(req);
     }
+}
+
+fn con_req(mut req: TcpStream) {
+    let mut buf = [0; 1024];
+    let len = req.read(&mut buf).unwrap();
+    println!("Request: {}", String::from_utf8_lossy(&buf[..len]));
 }
