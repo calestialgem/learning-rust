135eb841ac7f31008de8f9da81f9c5403be64bbe
Cem GEÇGEL <gecgelcem@outlook.com>
2022-02-04 09:38:22 +0300

    Channels

--- threads/src/main.rs
+++ threads/src/main.rs
@@ -1,16 +1,13 @@
+use std::sync::mpsc;
 use std::thread;
-use std::time::Duration;
 
 fn main() {
-    thread::spawn(|| {
-        for i in 1..10 {
-            println!("hi number {} from the spawned thread!", i);
-            thread::sleep(Duration::from_millis(1));
-        }
+    let (tx, rx) = mpsc::channel(); // multiple producer, single consumer
+    thread::spawn(move || {
+        let val = String::from("hi");
+        tx.send(val).unwrap();
     });
 
-    for i in 1..5 {
-        println!("hi number {} from the main thread!", i);
-        thread::sleep(Duration::from_millis(1));
-    }
+    let received = rx.recv().unwrap();
+    println!("Got: {}", received);
 }
