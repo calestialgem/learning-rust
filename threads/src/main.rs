7bea3f5f47c334bb3c70d9a51d527b3e1d4a718d
Cem GEÇGEL <gecgelcem@outlook.com>
2022-02-04 09:43:24 +0300

    Receiver as Iterator

--- threads/src/main.rs
+++ threads/src/main.rs
@@ -1,13 +1,25 @@
 use std::sync::mpsc;
 use std::thread;
+use std::time::Duration;
 
 fn main() {
     let (tx, rx) = mpsc::channel(); // multiple producer, single consumer
     thread::spawn(move || {
-        let val = String::from("hi");
-        tx.send(val).unwrap();
+        let vals = vec![
+            String::from("hi"),
+            String::from("from"),
+            String::from("the"),
+            String::from("thread"),
+        ];
+        for val in vals {
+            tx.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
     });
 
-    let received = rx.recv().unwrap();
-    println!("Got: {}", received);
+    // iteration ends when the channel is closed
+    // until then it waits for any data
+    for received in rx {
+        println!("Got: {}", received);
+    }
 }
